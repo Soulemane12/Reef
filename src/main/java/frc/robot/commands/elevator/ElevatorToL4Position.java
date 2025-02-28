d56e3bd1d8a798620f70bc246bbae7a972738201
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-02-28 16:19:29 -0500

    feat: shoot for certain duration once reaching L4

--- src/main/java/frc/robot/commands/elevator/ElevatorToL4Position.java
+++ src/main/java/frc/robot/commands/elevator/ElevatorToL4Position.java
@@ -1,10 +1,13 @@
 package frc.robot.commands.elevator;
 
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.subsystems.Shooter;
 
 public class ElevatorToL4Position extends ElevatorPositionCommandBase {
     private static Shooter m_shooter;
     private boolean positionReached = false;
+    private final Timer shootTimer = new Timer();
+    private static final double SHOOT_DURATION = 1.0;
 
     public static void initialize(Shooter shooter) {
         m_shooter = shooter;
@@ -22,6 +25,12 @@ public class ElevatorToL4Position extends ElevatorPositionCommandBase {
         if (Math.abs(m_elevator.getPosition() - 4.5) < 0.1 && !positionReached) {
             positionReached = true;
             m_shooter.shoot(1.0);
+            shootTimer.restart();
+        }
+
+        // Stop shooting after SHOOT_DURATION seconds
+        if (positionReached && shootTimer.hasElapsed(SHOOT_DURATION)) {
+            m_shooter.shoot(0.0);
         }
     }
 
@@ -29,5 +38,6 @@ public class ElevatorToL4Position extends ElevatorPositionCommandBase {
     public void end(boolean interrupted) {
         m_shooter.shoot(0.0);
         positionReached = false;
+        shootTimer.stop();
     }
 }
