9e3bcc516fa9339616e061472d01b546c97f85c3
Soulemane Sow <soulemanesow10@gmail.com>
2025-03-01 11:04:06 -0500

    chnage from neo to kraken for shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,12 +1,12 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
   // Instantiate the motor on PWM/Can ID 10 (adjust if needed) as brushless.
-  private final SparkMax motor = new SparkMax(30, MotorType.kBrushless);
+  private final TalonFX motor =new TalonFX(30, "ChooChooTrain");
+
 
   /** Sets the shooter motor speed.
    * @param speed The speed to set. Positive values shoot forward.
