9f5aef3d09ec4a0b86c7a29c6ac0d1267e37ccb5
Soulemane Sow <14soulemanesow@gmail.com>
2025-03-01 21:56:36 -0500

    joy to driver b to op

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -48,9 +48,8 @@ public class RobotContainer {
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
-    private final CommandXboxController joystick = new CommandXboxController(0); //driver
-    private final CommandXboxController joysticks = new CommandXboxController(1); 
-    private final CommandXboxController joysticksb = new CommandXboxController(2); //operator
+    private final CommandXboxController driver = new CommandXboxController(0); //driver
+    private final CommandXboxController operator = new CommandXboxController(1); //operator
 
     
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
@@ -123,70 +122,70 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
+                drive.withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(-driver.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        driver.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        driver.b().whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))
         ));
 
-        joystick.pov(0).whileTrue(drivetrain.applyRequest(() ->
+        driver.pov(0).whileTrue(drivetrain.applyRequest(() ->
             forwardStraight.withVelocityX(0.5).withVelocityY(0))
         );
-        joystick.pov(180).whileTrue(drivetrain.applyRequest(() ->
+        driver.pov(180).whileTrue(drivetrain.applyRequest(() ->
             forwardStraight.withVelocityX(-0.5).withVelocityY(0))
         );
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        driver.back().and(driver.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        driver.back().and(driver.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        driver.start().and(driver.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        driver.start().and(driver.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // reset the field-centric heading on left bumper press
-        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+        driver.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
-        joysticks.rightTrigger().whileTrue(new RunCommand(() -> shooter.shoot(1.0), shooter))
+        driver.rightTrigger().whileTrue(new RunCommand(() -> shooter.shoot(1.0), shooter))
                                 .onFalse(new InstantCommand(() -> shooter.shoot(0.0), shooter));
 
         // Elevator default command
         m_elevator.setDefaultCommand(
-            new ElevatorJoystickCommand(m_elevator, joysticks, kElevatorGravityCompensation)
+            new ElevatorJoystickCommand(m_elevator, driver, kElevatorGravityCompensation)
         );
 
         // Elevator position control with buttons
-        joysticksb.a().onTrue(m_elevatorToL2Position);
-        joysticksb.b().onTrue(m_elevatorToL3Position);
-        joysticksb.y().onTrue(m_elevatorToL4Position);
+        operator.a().onTrue(m_elevatorToL2Position);
+        operator.b().onTrue(m_elevatorToL3Position);
+        operator.y().onTrue(m_elevatorToL4Position);
 
         //Climber Control
-        joysticksb.x().onTrue(m_elevatorTo0Position);
+        operator.x().onTrue(m_elevatorTo0Position);
 
         // Position default command
         m_Pivot.setDefaultCommand(
-            new PositionJoystickCommand(m_Pivot, joysticks, kPositionGravityCompensation)
+            new PositionJoystickCommand(m_Pivot, driver, kPositionGravityCompensation)
         );
 
-        joysticksb.rightTrigger().onTrue(m_pivotToL2L3);
-        joysticksb.leftTrigger().onTrue(m_pivotTo0);
+        operator.rightTrigger().onTrue(m_pivotToL2L3);
+        operator.leftTrigger().onTrue(m_pivotTo0);
         
-        joysticksb.leftTrigger().onTrue(m_pivotToL4);
+        operator.leftTrigger().onTrue(m_pivotToL4);
         
         //NEW METHODS FOR TESTING
-        //joysticksb.x().onTrue(m_pivotTo0.andThen(m_elevatorTo0Position));
-        //joysticksb.a().onTrue(m_pivotToL2L3.andThen(m_elevatorToL2Position));
-        //joysticksb.b().onTrue(m_pivotToL2L3.andThen(m_elevatorToL3Position));
-        //joysticksb.y().onTrue(m_pivotToL4.andThen(m_elevatorToL4Position));
+        operator.x().onTrue(m_pivotTo0.andThen(m_elevatorTo0Position));
+        //operator.a().onTrue(m_pivotToL2L3.andThen(m_elevatorToL2Position));
+        //operator.b().onTrue(m_pivotToL2L3.andThen(m_elevatorToL3Position));
+        //operator.y().onTrue(m_pivotToL4.andThen(m_elevatorToL4Position));
     
 
         //Shooter Control
-        joysticksb.leftBumper().whileTrue(shooter.shooterIntakeControl());
-        joysticksb.rightBumper().onTrue(shooter.shooterOutakeControl().withTimeout(0.75));
+        operator.leftBumper().whileTrue(shooter.shooterIntakeControl());
+        operator.rightBumper().onTrue(shooter.shooterOutakeControl().withTimeout(0.75));
 
 
         drivetrain.registerTelemetry(logger::telemeterize);
